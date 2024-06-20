7a86fb6529396388cf3aba37fc0b7f48878210f1
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2024-06-20 14:13:51 +0300

    fixed sitemap error

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,11 +3,13 @@ import {fetchProducts} from "@/lib/fetch-products/getAllProducts";
 import {IProduct} from "@/types/Product";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const data = await fetchProducts(20,1);
-    
     const baseUrl = 'https://next-test-project-ruby.vercel.app';
 
-    return data.products.map((product:IProduct) => ({
+    const data = await fetch(`${baseUrl}/api/products?limit=20&page=1`);
+
+    const {products} = await data.json();
+
+    return products.map((product:IProduct) => ({
         url:`${baseUrl}/product/${product.id}`,
         lastModified:new Date(Date.now())
     }))
