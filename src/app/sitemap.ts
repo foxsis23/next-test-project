62f12b67dbe3d5d24aa91a73500cc357d019666f
Illia Strilets <56593640+foxsis23@users.noreply.github.com>
2024-06-20 13:30:40 +0300

    added sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,20 @@
-// import { MetadataRoute } from 'next'
-// import {fetchProducts} from "@/lib/fetch-products/getAllProducts";
-// import {IProduct} from "@/types/Product";
-//
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//     const data = await fetchProducts(20,1);
-//
-//
-//     return data.products.map((product:IProduct) => ({
-//         url:`http://localhost:3000/product/${product.id}`,
-//         lastModified:new Date(Date.now())
-//     }))
-// }
+import { MetadataRoute } from 'next'
+import {fetchProducts} from "@/lib/fetch-products/getAllProducts";
+import {IProduct} from "@/types/Product";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    // const data = await fetchProducts(20,1);
+    //
+    //
+    // return data.products.map((product:IProduct) => ({
+    //     url:`http://localhost:3000/product/${product.id}`,
+    //     lastModified:new Date(Date.now())
+    // }))
+
+    return [
+        {
+            url:`http://localhost:3000/product/`,
+            lastModified:new Date(Date.now())
+        }
+    ]
+}
